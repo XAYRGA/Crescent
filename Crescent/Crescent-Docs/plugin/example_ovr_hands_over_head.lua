f2f28bc3c96dba5de7df866c85cfaa57a939836e
XAYRGA <dzbush25@gmail.com>
2023-07-31 22:59:28 -0400

    hands-over-head example

---
+++ Crescent/Crescent-Docs/plugin/example_ovr_hands_over_head.lua
@@ -0,0 +1,26 @@
+﻿AUTHOR = "xayrga"
+NAME = "hands over head"
+GUID = "1a07395a-4722-4769-95ea-f5bda763f683"
+
+local debounce = 0 
+
+local BUTTON_A = 2 
+-- Coordinate X = Left Right, Y = Up Down , Z = Forward Back
+event.subscribe("update","myUpdate",function()
+	
+	local lHandPos = ovr.getTrackerPosition( ovr.getLeftHand() )
+	local rHandPos = ovr.getTrackerPosition( ovr.getRightHand() )
+	local HMDPos = ovr.getTrackerPosition( ovr.getHMD() )
+
+	-- Check if the player has their hands over their head. 
+	if ( (lHandPos.Y > HMDPos.Y) and (rHandPos.Y > HMDPos.Y) ) then 
+
+		local dist = lHandPos.Y - HMDPos.Y
+
+		print("left hand is above head!!! " .. dist )
+		-- Writes the "Expression-Scared parameter to your avatar as true."
+		avatar.setBool("Expression-Scared",true)
+	end 
+
+end)
+
